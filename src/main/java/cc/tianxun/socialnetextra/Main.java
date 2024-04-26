cccf17700ff3386e0f288584f6cc6695c7f10dae
GitTIanxun163 <tianxunwx@163.com>
2024-04-26 19:23:27 +0800

    增加killself指令

--- src/main/java/cc/tianxun/socialnetextra/Main.java
+++ src/main/java/cc/tianxun/socialnetextra/Main.java
@@ -1,5 +1,6 @@
 package cc.tianxun.socialnetextra;
 
+import cc.tianxun.socialnetextra.command.*;
 import cc.tianxun.socialnetextra.socials.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -87,11 +88,14 @@ public final class Main extends JavaPlugin {
         // 注册命令/事件
         System.out.println("Registering events/commands.");
         PlayerTp playerTp = new PlayerTp();
+        Kill kill = new Kill();
         Objects.requireNonNull(getCommand("tpa")).setExecutor(playerTp);
         Objects.requireNonNull(getCommand("tpac")).setExecutor(playerTp);
         Objects.requireNonNull(getCommand("tpde")).setExecutor(playerTp);
         Objects.requireNonNull(getCommand("tpnel")).setExecutor(playerTp);
+        Objects.requireNonNull(getCommand("kill")).setExecutor(kill);
         getServer().getPluginManager().registerEvents(playerTp,this);
+        getServer().getPluginManager().registerEvents(kill,this);
     }
 
     @Override
---
+++ src/main/java/cc/tianxun/socialnetextra/command/Kill.java
@@ -0,0 +1,31 @@
+package cc.tianxun.socialnetextra.command;
+
+import cc.tianxun.socialnetextra.Main;
+import org.bukkit.command.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.*;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class Kill implements CommandExecutor, Listener {
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("§4Only players can use this command.");
+			return true;
+		}
+		Player player = (Player)sender;
+		if (!Main.getInstance().getConfig().getBoolean("enable_killself")) {
+			player.sendMessage(Main.getInstance().getLangKey("command.killself.disabled_message",player.getLocale()));
+			return true;
+		}
+
+		player.damage(Double.POSITIVE_INFINITY);
+		return true;
+	}
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		if (event.getEntity().getKiller() == null) {
+			event.setDeathMessage(event.getEntity().getName() + "自杀了");
+		}
+	}
+}
