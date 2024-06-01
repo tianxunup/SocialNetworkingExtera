665303ebe0bc9af34935a91c9c48bcad1420861a
GitTIanxun163 <tianxunwx@163.com>
2024-06-01 16:23:35 +0800

    玩家称号管理

---
+++ src/main/java/cc/tianxun/socialnetextra/socials/PlayerPrefix.java
@@ -0,0 +1,12 @@
+package cc.tianxun.socialnetextra.socials;
+
+import org.bukkit.command.*;
+
+public class PlayerPrefix implements CommandExecutor {
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// ....
+		return true;
+	}
+}
+
